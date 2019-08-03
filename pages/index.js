c0a80c5d282769a6c3a62db84031863e971057e2
Aldren Terante <aldren.terante@gmail.com>
2019-08-03 19:43:16 +0800

    feat: use antd as ui frameworkOD

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,43 @@
-import HelloWorld from '../components'
-export default () => (
-  <div>
-    <h1>Simple Storybook Example</h1>
-    <HelloWorld />
-  </div>
-)
+
+import * as React from 'react';
+import stylesheet from 'antd/dist/antd.min.css'
+
+import { Layout, Menu } from 'antd';
+const { Header, Content } = Layout;
+
+export default class App extends React.Component {
+  render() {
+    return <Layout>
+      <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+      <style jsx>{`
+        #components-layout-demo-top-side-2 .logo {
+          width: 120px;
+          height: 31px;
+          background: #333;
+          border-radius: 6px;
+          margin: 16px 28px 16px 0;
+          float: left;
+        }
+      `}</style>
+
+      <Header className='header'>
+        <div className='logo' />
+        <Menu
+          theme='dark'
+          mode='horizontal'
+          defaultSelectedKeys={['1']}
+          style={{ lineHeight: '64px' }}
+        >
+          <Menu.Item key='1'>Home</Menu.Item>
+        </Menu>
+      </Header>
+      <Layout>
+        <Layout style={{ padding: '24px', minHeight: 'calc(100vh - 64px)' }}>
+          <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 280 }}>
+            Content
+          </Content>
+        </Layout>
+      </Layout>
+    </Layout>
+  }
+}
