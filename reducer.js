ca6c2c58481b856183e8f8b7427e98e9d8af6c20
Aldren Terante <aldren.terante@gmail.com>
2019-08-04 16:38:27 +0800

    feat: initialize and integrate redux-saga

---
+++ reducer.js
@@ -0,0 +1,12 @@
+import { actionTypes } from './actions'
+
+export const appInitialState = {}
+
+function reducer (state = appInitialState, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default reducer
